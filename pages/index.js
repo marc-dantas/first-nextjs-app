f353b7ed73e46e2edf2410fd5c6f838a3fbd742f
marc-dantas <marciodantasms@hotmail.com>
2022-12-15 20:03:59 -0300

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Index() {
+    return (
+        <div>
+            <h1>Hello world!</h1>
+            <p>I guess it it my first project in the React.js (Next.js) framework!</p>
+        </div>
+    );
+}
